f89b2b6834e76ee2341fdbda7995518b9c3064ae
Francisco Kahil <codexico@gmail.com>
2016-01-26 16:22:20 -0200

    Add facebook login

--- app/src/main/java/com/example/fk/gtd_crud/LoginActivity.java
+++ app/src/main/java/com/example/fk/gtd_crud/LoginActivity.java
@@ -4,9 +4,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,16 +36,60 @@ public class LoginActivity extends AppCompatActivity {
     @Bind(R.id.cbStay_signed_in)
     CheckBox cbStay_signed_in;
 
+    private TextView info;
+    private LoginButton fbLoginButton;
+    private CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
 
         ButterKnife.bind(this);
 
-        if (isSignedIn()) {
+        info = (TextView)findViewById(R.id.info);
+        fbLoginButton = (LoginButton)findViewById(R.id.fbLoginButton);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        if (isSignedIn() || isLoggedInFB()) {
             goToMainList();
         }
+
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+                goToMainList();
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
+    }
+    public boolean isLoggedInFB() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @OnClick(R.id.btSignIn)
